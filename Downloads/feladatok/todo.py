7002b738d89c49f4641a1614fa571ebe22a91f95
sador23 <adorjns@gmail.com>
2016-10-18 10:51:38 +0200

    legujabb

--- Downloads/feladatok/todo.py
+++ Downloads/feladatok/todo.py
@@ -1,15 +1,10 @@
-
-
-#commands={'add': "add","liste":"liste","mark":"mark","archive":"archive"}
 commands=["add","liste","mark","archive"]
+todolist=[]
+marklist=[]
+marked="[x]"
+notmarked="[]"
 
-class todo(): 
-    todolist=[]
-    marklist=[]
-    marked="[x]"
-    notmarked="[]"
-
-    def add():
+def add():
         a=input("add an item:")
         todolist.append(a)
         len(todolist)-1
@@ -17,29 +12,38 @@ class todo():
         len(notmarked)-1
         print("Item added.")
 
-    def liste():
+def liste():
         print("You saved the following to-do items:")
         for i in range(0,len(marklist)):
             print(str(i+1) + " " + str(marklist[i]) + " " + todolist[i])
 
-    def mark():
+def mark():
+        print ('You saved the following to-do items:')
+        liste ()
+        i = input ('Which one you want to mark as completed: ')
+        marklist[i] = marked
+        print (str(todolist[i]) + ' is completed')
 
 
-
-    def archive():
+def archive():
         for i in range(0,len(marklist)-1):
             if marklist[i]==marked:
                 todolist.remove(i)
                 marked.remove(i)
                 print("You saved the following items : ")
                 liste()
-        print("archive")
     
-    def command_input():
+def command_input():
         while True:
             command=input("Please enter a command [add,liste,mark,archive]: ")
             if command in commands:
-                getattr(todo,command)()
+                if command=="add":
+                    add()
+                elif command =="liste":
+                    liste()
+                elif command=="mark":
+                    mark()
+                elif command=="archive":
+                    archive()
 
-
-todo.command_input()
+command_input()
